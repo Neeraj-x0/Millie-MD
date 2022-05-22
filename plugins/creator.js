15df752a6549a3295b9ff8c9768d13ade9671857
Neeraj <95992247+Neeraj-x0@users.noreply.github.com>
2022-05-22 14:55:30 +0530

    `Millie-MD`

--- plugins/creator.js
+++ plugins/creator.js
@@ -19,11 +19,10 @@ let handler = async(m, { conn }) => {
   let audio = pickRandom(sound)
   let msg = "🎈ɴᴏ ᴄᴀʟʟs ᴏɴʟʏ ᴍᴇssᴀɢᴇ"
   let name= m.sender
-  let mail = 'krishnaneeraj775@gmail.com'
   let verfication = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: 'status@broadcast' } : {}) }, message: { contactMessage: { displayName: `${await conn.getName(name)}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
    try {
 await conn.sendContactArray(m.chat, [
-    [`${owner[0]}`, `ɴᴇᴇʀᴀᴊ`,`Just a Chad with a curios Mind`,msg,mail]
+    [`${owner[0]}`, `owner_name`,'',msg,null]
 ],verfication) 
 await conn.sendFile(m.chat,audio, 'file.mp4', '', m, 1, { mimetype: 'audio/mp4' },verfication)
    } catch {
