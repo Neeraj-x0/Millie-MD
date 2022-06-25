0214ae1586a2aa609933af3113de7d626124d1a7
KHADHER756 <108067283+KHADHER756@users.noreply.github.com>
2022-06-25 07:32:49 +0530

    kdr

--- plugins/credits.js
+++ plugins/credits.js
@@ -3,20 +3,10 @@ let credit = `
 \t\t\t\t\t\t\t*THANKS TO*
 
 *TOXIC ALIEN:*
-*https://github.com/Alien-alfa*
-
-*D-3-V-1-L:*
-*https://github.com/D-3-V-1-L*
-
-*Toxic-Ajmal:*
-*https://github.com/Ajmal-Achu*
-
-*Lord-Official:*
-*https://github.com/Lord-official*
-
-*AND MYSELF*
+      &
+  *KHADHER*
 `
- conn.sendTBI(m.chat, credit, wm, 'https://i.pinimg.com/474x/de/bd/b2/debdb24645169bf95eecd49f3144315f.jpg', `SOURCE CODE`, `https://github.com/Neeraj-x0/Millie-MD`, null, null, 'Menu', `${usedPrefix}menu`, null, null, null, null, m) 
+ conn.sendTBI(m.chat, credit, wm, 'https://telegra.ph/file/5a7adde75e966077a7692.jpg', `SOURCE CODE`, `https://github.com`, null, null, 'Menu', `${usedPrefix}menu`, null, null, null, null, m) 
 }
 handler.help = ['credits']
 handler.tags = ['info']
