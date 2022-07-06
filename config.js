490a34f297dbf432abd7050c39d92eaf76eae8a1
RUBI-CON <108249082+RUBI-CON@users.noreply.github.com>
2022-07-06 08:49:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // Premium user
 
 global.devx = 'false'
 
-global.owner = ["918113921898"]
+global.owner = ["917034890656"]
 global.APIs = { // API Prefix
 //======= name: 'https://website' ========//
   nrtm: 'https://nurutomo.herokuapp.com',
