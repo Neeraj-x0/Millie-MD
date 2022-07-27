b448dc82955cebd236a68c5ac4dd57468054b85e
Neeraj <95992247+Neeraj-x0@users.noreply.github.com>
2022-07-27 19:17:44 +0530

    Add files via upload

---
+++ plugins/update.js
@@ -0,0 +1,68 @@
+const Heroku = require("heroku-client");
+let { tiny } = require("../lib");
+let config = require("../config");
+const heroku = new Heroku({ token: config.HEROKU_API_KEY });
+const simpleGit = require("simple-git");
+const git = simpleGit();
+module.exports = {
+  name: "update",
+  category: "owner",
+  desc: "Check for updates",
+  isQuoted: true,
+  async mbb({ msg, conn }, { q, prefix }) {
+    await git.fetch();
+    var commits = await git.log(["master" + "..origin/" + "master"]);
+    if (q === "now") {
+      if (commits.total === 0)
+        return await conn.sendMessage(m.chat, {
+          text: "```You have the latest version installed```",
+        });
+      var app = await heroku.get("/apps/" + config.HEROKU_APP_NAME);
+      await conn.sendMessage(m.chat, { text: "*ᴜᴘᴅᴀᴛɪɴɢ...*" });
+      git.fetch("upstream", config.BRANCH);
+      git.reset("hard", ["FETCH_HEAD"]);
+      var git_url = app.git_url.replace(
+        "https://",
+        "https://api:" + config.HEROKU_API_KEY + "@"
+      );
+      try {
+        try {
+          await git.addRemote("heroku", git_url);
+        } catch (e) {
+          console.log(e);
+        }
+
+        await git.push("heroku", config.BRANCH);
+
+        await conn.sendMessage(m.chat, { text: "*Updated...*" });
+
+        await conn.sendMessage(m.chat, { text: "Restarting..." });
+      } catch (e) {
+        m.reply(e);
+      }
+    }
+
+    if (commits.total === 0) {
+      await conn.sendMessage(m.chat, {
+        text: "```You have the latest version installed```",
+      });
+    } else {
+      var availupdate = "*ᴜᴘᴅᴀᴛᴇs ᴀᴠᴀɪʟᴀʙʟᴇ* \n\n" + "```";
+      commits["all"].map((commit, num) => {
+        availupdate += num + 1 + " ●  " + tiny(commit.message) + "\n";
+      });
+      await sendMessage(msg.from, {
+        text: availupdate,
+        footer: tiny("click here to update"),
+        buttons: [
+          {
+            buttonId: `${prefix}update now`,
+            buttonText: { displayText: tiny("update now") },
+            type: 1,
+          },
+        ],
+        headerType: 1,
+      });
+    }
+  },
+};
