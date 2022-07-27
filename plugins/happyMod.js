9e382fcfbccd9174cb43cd05db2e8dfbca15734e
Neeraj <95992247+Neeraj-x0@users.noreply.github.com>
2022-07-27 23:07:31 +0530

    Update happyMod.js

--- plugins/happyMod.js
+++ plugins/happyMod.js
@@ -4,7 +4,7 @@ module.exports = {
 	category: "Search",
 	desc: "Do a search in happy Mod",
     query:"Enter Search Term",
-	async mbb({ msg,conn},{args}) {
+	async mbb({ msg,conn},{q}) {
         let template = await hmodtemplate(q)
         if (template=== 404 ) return msg.reply("No result Found")
         await conn.sendMessage(msg.from, template);
