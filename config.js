fbf14384838fdddd9218b34a0af27f81b3f119f9
Neeraj <95992247+Neeraj-x0@users.noreply.github.com>
2022-07-31 13:44:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,10 +8,12 @@ module.exports = {
   bot_name: process.env.BOT_NAME || "Millie-MD",
   timezone: process.env.TIME_ZONE || "Asia/Kolkata",
   locale: "in",
-  Session_Id: process.env.SESSION_ID || "",
-  HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
-  HEROKU_API_KEY: process.env.HEROKU_API_KEY,
+  Session_Id: process.env.SESSION_ID || "U2_Fs_dG_Vk_X1_8L_Jr_3k_/3_HG_M4_ce_1d_1g_m+_IC_mR_Hc_5C_x9_kN_E;millie",
+  HEROKU_APP_NAME: process.env.HEROKU_APP_NAME||'millietest',
+  HEROKU_API_KEY: process.env.HEROKU_API_KEY||'23dfcbe6-d1a1-4b17-a94b-ce9c3dbbc9d7',
+  DB_URL:process.env.DATABASE_URL,
   BRANCH: "main",
+  AI : process.env.AI||false
 };
 function ToBool(text, fault = "true") {
   return text === fault ? true : false;
