89f29eda9109c1f2638d38d69cc82903bcf22f9f
Neeraj <95992247+Neeraj-x0@users.noreply.github.com>
2022-08-03 14:07:49 +0530

    Update image.js

--- plugins/image.js
+++ plugins/image.js
@@ -1,1 +1,16 @@
 
+const {gimage} = require('../lib')
+module.exports = {
+	name: "img",
+	category: "Search",
+	desc: "Google Image search",
+    query:"Enter Search Term,number",
+	async mbb({ msg,conn},{q}) {
+        let [query,amount] = q.split(',')
+        let result = await gimage(query,amount)
+        conn.sendMessage(msg.from,{text:`_Downloading ${amount||5} images for ${query}_`})
+        for (let i of result){
+            conn.sendMessage(msg.from,{image:{url:i}})
+        }
+	}
+}
