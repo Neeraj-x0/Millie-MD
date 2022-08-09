568bdbc393f56356779b7cd93709c0446a6b24ec
Neeraj <95992247+Neeraj-x0@users.noreply.github.com>
2022-08-09 18:10:45 +0530

    Update yts.js

--- plugins/yts.js
+++ plugins/yts.js
@@ -1,13 +1,17 @@
-const { ytstemplate} = require("../lib");
+const { yts} = require("../lib");
 module.exports = {
   name: "ytsearch",
   alias: ["yts"],
   category: "Search",
   desc: "Yotube search",
   query: "Give A Search term",
-  wait: true,
   async mbb({ msg, conn }, { q, prefix }) {
-    let list = await ytstemplate(q, prefix);
-    await conn.sendMessage(msg.from, list);
+    yts(q).then(async ({video})=>{
+      let str =``
+      video.map(({authorName,url,title,durationH,viewH,publishedTime},num)=>{
+        str+=`${num+1} ${title}\nViews : ${viewH}\nTime : ${durationH}\nPublished : ${publishedTime}\nChannel : ${authorName}\nURL : ${url} \n\n`  })
+        await conn.sendMessage(msg.from, {text:str.trim()});
+    })
+    
   },
 };
